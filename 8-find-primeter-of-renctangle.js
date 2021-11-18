01a92af3e334a25f374a036cb6d4c65163e35b49
yarentamir <yarden.tamir@walla.co.il>
2021-11-18 15:42:47 +0200

    8 primeter of req

---
+++ 8-find-primeter-of-renctangle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const reqPerimeter = (length, width) => (length + width) * 2;
+
+console.log(reqPerimeter(6, 7));
+console.log(reqPerimeter(20, 10));
+console.log(reqPerimeter(2, 9));
