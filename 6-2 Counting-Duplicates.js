368239585820174599e282dcbdc172ed3b133ed5
yarentamir <yarden.tamir@walla.co.il>
2021-11-18 15:59:19 +0200

    added reduce function to 6-2 and 6-4

--- 6-2 Counting-Duplicates.js
+++ 6-2 Counting-Duplicates.js
@@ -1,5 +1,6 @@
 "use strict";
 
+console.log("--- no reduce ---");
 const duplicateCount = (text) => {
   const newText = text.toLowerCase();
   let count = 0;
@@ -17,3 +18,23 @@ const duplicateCount = (text) => {
 console.log(duplicateCount("abcde"));
 console.log(duplicateCount("aA11"));
 console.log(duplicateCount("ABBA"));
+
+console.log("--- with reduce ---");
+
+const duplicateCount2 = (text) => {
+  const textArr = text.toLowerCase().split("");
+  let count = 0;
+  let result = textArr.reduce((obj, char) => {
+    !obj[char] ? (obj[char] = 1) : obj[char]++;
+    return obj;
+  }, {});
+
+  for (let i in result) {
+    if (result[i] > 1) count++;
+  }
+  return count;
+};
+
+console.log(duplicateCount2("abcde"));
+console.log(duplicateCount2("aA11"));
+console.log(duplicateCount2("ABBA"));
--- 6-4-Isogram.js
+++ 6-4-Isogram.js
@@ -1,5 +1,7 @@
 "use strict";
 
+console.log("--- no reduce ---");
+
 const isIsogramFunc = (str) => {
   const newText = str.toLowerCase();
   let isIsogram = true;
@@ -19,3 +21,26 @@ const isIsogramFunc = (str) => {
 console.log(isIsogramFunc("Dermatoglyphics"));
 console.log(isIsogramFunc("aba"));
 console.log(isIsogramFunc("moOse"));
+
+console.log("--- with reduce ---");
+
+const isIsogramFunc2 = (text) => {
+  const textArr = text.toLowerCase().split("");
+  let isIsogram = true;
+  let result = textArr.reduce((obj, char) => {
+    !obj[char] ? (obj[char] = 1) : obj[char]++;
+    return obj;
+  }, {});
+
+  for (let i in result) {
+    if (result[i] > 1) {
+      isIsogram = false;
+      break;
+    }
+  }
+  return isIsogram;
+};
+
+console.log(isIsogramFunc2("Dermatoglyphics"));
+console.log(isIsogramFunc2("aba"));
+console.log(isIsogramFunc2("moOse"));
